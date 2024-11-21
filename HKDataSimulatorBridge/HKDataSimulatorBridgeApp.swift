9995d3a4ebe9f50dd025b5aa3311898128cdecd8
HanryYu <fishion@outlook.com>
2024-11-21 14:18:39 +0800

    Initial Commit

---
+++ HKDataSimulatorBridge/HKDataSimulatorBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKDataSimulatorBridgeApp.swift
+//  HKDataSimulatorBridge
+//
+//  Created by Henry on 2024/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HKDataSimulatorBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
